d584469210a9b88f1409d2bf68c0f7598f209e81
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-05-20 04:40:27 +0530

    api

---
+++ PROJECT-01/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const users = require('./MOCK_DATA.json')
+const app = express();
+
+const PORT = 8000
+
+//ROUTES
+app.get('/users',(req,res) =>{
+    const html = `
+    <ul>
+    ${users.map((user)=>`<li>${user.first_name}</li>`).join("") }
+    </ul>
+    `;
+    res.send(html)
+})
+
+app.get('/api/users',(req,res) =>{
+    return res.json(users);
+})
+
+app.route('/api/users/:id').get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id === id);
+    return res.json(user)
+}).patch((req,res)=>{
+    //Edit user with id
+    return res.json({status:'Pending'})
+})
+.delete((req,res)=>{
+    //delete user with id
+    return res.json({status:'Pending'})
+}); 
+
+
+app.post('/api/users',(req,res)=>{
+    //TODO: Create new user
+    return res.join({status:"pending"})
+});
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server Started at port:${PORT}`))
