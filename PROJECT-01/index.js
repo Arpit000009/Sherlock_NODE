531099b845bb3a2c7f4fc7f5bc40776aad060bb8
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-05-21 22:36:01 +0530

    add user

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const fs = require('fs')
 const users = require('./MOCK_DATA.json')
 const app = express();
 
 const PORT = 8000
+app.use(express.urlencoded({extended: false}))
 
 //ROUTES
 app.get('/users',(req,res) =>{
@@ -34,7 +36,11 @@ app.route('/api/users/:id').get((req,res)=>{
 
 app.post('/api/users',(req,res)=>{
     //TODO: Create new user
-    return res.join({status:"pending"})
+    const body = req.body;
+    users.push({...body, id: users.length+1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+        return res.json({status:"success",id: users.length });
+    })
 });
 
 
