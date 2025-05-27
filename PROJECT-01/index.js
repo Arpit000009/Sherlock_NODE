977a22a860ae3797f54b5d7e8bc6a6d5cf4c19bf
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-05-28 03:27:11 +0530

    mongodb

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const fs = require('fs')
-const users = require('./MOCK_DATA.json')
+const users = require('./MOCK_DATA.json');
+const { default: mongoose } = require("mongoose");
 const app = express();
 
 const PORT = 8000
+
+//Connection
+mongoose
+.connect('mongodb://127.0.0.1:27017/user-app-1')
+.then(() =>console.log("mongoDB connected"))
+.catch(err=>console.log("Mongo error",err));
+
+// Schema
+const userSchema = new mongoose.Schema({
+    firstName:{
+        type:String,
+        required: true,
+    },
+    lastName:{
+        type:String,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique: true,
+    },
+    jobTitle:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    gender:{
+        type: String,
+    }
+},{timestamps: true});
+
+const User = mongoose.model('user',userSchema);
+
 //middleware -- plugin
 app.use(express.urlencoded({extended: false}))
 
@@ -49,16 +83,28 @@ app.route('/api/users/:id').get((req,res)=>{
 }); 
 
 
-app.post('/api/users',(req,res)=>{
+app.post('/api/users',async(req,res)=>{
     //TODO: Create new user
     const body = req.body;
-    if(!body ||!body.first_name || !body.last_name|| !body.email ||!body.gender){
-        return res.status(400).json({msg:"all fields are required"})
+    if(!body || 
+       !body.first_name ||
+       !body.last_name||
+       !body.email ||
+       !body.gender ||
+       !body.job_title
+    ){
+        return res.status(400).json({msg:"all fields are required"});
     }
-    users.push({...body, id: users.length+1});
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
-        return res.status(201).json({status:"success",id: users.length });
+
+   const result =  await User.create({
+        firstName:body.first_name,
+        lastName:body.last_name,
+        email:body.email,
+        gender:body.gender,
+        jobTitle:body.job_title
     })
+
+   return res.status(201).json({msg:"sucess"})
 });
 
 
