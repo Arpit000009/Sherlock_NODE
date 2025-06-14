ea91635ff76637d641f10e06efae9fc243a6d9a8
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-06-14 23:13:35 +0530

    interface

--- SHORT-URL/routes/staticRouter.js
+++ SHORT-URL/routes/staticRouter.js
@@ -5,7 +5,9 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   if (!req.user) return res.redirect("/login");
   const allurls = await URL.find({ createdBy: req.user._id });
-  return res.render("home", { urls: allurls });
+  return res.render("home", { urls: allurls,
+    userName: req.user.name
+   });
 });
 
 router.get("/signup", (req, res) => res.render("signup"));
