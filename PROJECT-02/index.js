16fed18791c59a3a5a0c344cfe43bfe992746ce6
Arpit000009 <100606591+Arpit000009@users.noreply.github.com>
2025-07-06 00:02:11 +0530

    initilization

--- PROJECT-02/index.js
+++ PROJECT-02/index.js
@@ -1,1 +1,14 @@
-console.log("run");
+const http = require("http");
+const express = require('express');
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/",(req,res)=>{
+    return res.sendFile("/public/index.html")
+})
+
+server.listen(9000,()=>console.log(`Server Started at PORT:9000`))
